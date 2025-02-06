503154f8cc7ccfbd2b89a02cb49fddb820c8d6dc
slowkow <kslowikowski@gmail.com>
2025-02-06 01:04:50 +0000

    Deploying to gh-pages from @ slowkow/hlabud@466aeaafb49049cd761ce0bc12d3cc355e76eb15 🚀

--- articles/visualize-hla-structure_files/helperFunctions-1/NGLVieweR.js
+++ articles/visualize-hla-structure_files/helperFunctions-1/NGLVieweR.js
@@ -7,7 +7,7 @@ HTMLWidgets.widget({
   factory: function(el, width, height) {
 
       var stage = new NGL.Stage(el);
-      var structure = null;
+      var structures = null;
 
     return {
       renderValue: function(opts) {
@@ -25,7 +25,7 @@ HTMLWidgets.widget({
       stage.handleResize();
       }, false );
       }
-
+   
       //Load timer for rendering
       //console.time("load-to-render");
 
@@ -33,86 +33,181 @@ HTMLWidgets.widget({
       stage.setParameters(opts.stageParameters);
       stage.setQuality(opts.setQuality);
       stage.setFocus(opts.setFocus);
-
-      //Load pdb from file or PDB identifier
-      if(opts.type == "code"){
-        structure = stage.loadFile(opts.data);
-      }
-      if (opts.type == "file"){
-        structure = stage.loadFile(new Blob([opts.data], {type: 'text/plain'}), {
-         ext:opts.file_ext});
-      }
       
-      //Load representation inputs
-      var representation = opts.addRepresentation;
-      var arrType = representation.type;
-      var arrValues = representation.values;
-      //Convert array values
-      arrHandler(arrValues, "color");
+      // Iterate through all structures in the list
+        Promise.all(opts.structures.map(function(structureOpts) {
+          if (structureOpts.type == "code") {
+            return stage.loadFile(structureOpts.data);
+          } else if (structureOpts.type == "file") {
+            return stage.loadFile(new Blob([structureOpts.data], { type: 'text/plain' }), {
+             ext: structureOpts.file_ext
+            });
+          }
+        })).then(function(loaded) {
+          
+          // Make globaly available
+          structures = loaded;
+          
+          // Initialize arrays to collect data and send to Shiny
+          var sequences = [];
+          var resnos = [];
+          var chainnames = [];
+          var PDBdatas = [];
 
-      structure.then(function(o){
-      //Apply to all values objects
-      arrValues.forEach((value, index) => {
-         let type = arrType[index];
-         o.addRepresentation(type, value)
-       });
+          structures.forEach(function(o, index) {
+            var structureOpts = opts.structures[index];
+
+            // Load representation inputs
+            var representation = structureOpts.addRepresentation;
+            var arrType = representation.type;
+            var arrValues = representation.values;
+            arrHandler(arrValues, "color");
+
+            // Apply to all representation values objects
+            arrValues.forEach((value, repIndex) => {
+              let type = arrType[repIndex];
+              o.addRepresentation(type, value);
+            });
+            
+            o.autoView();
+            stage.autoView();
+
+            // Set Scale
+            if (structureOpts.setScale) {
+              o.setScale(structureOpts.setScale);
+            }
+
+            // Set Rotation
+            if (structureOpts.setRotation && Object.keys(structureOpts.setRotation).length > 0) {
+              o.setRotation([structureOpts.setRotation.x, structureOpts.setRotation.y, structureOpts.setRotation.z]);
+            }
+
+            // Set Position
+            if (structureOpts.setPosition && Object.keys(structureOpts.setPosition).length > 0) {
+              o.setPosition([structureOpts.setPosition.x, structureOpts.setPosition.y, structureOpts.setPosition.z]);
+            }
+ 
+            // Set zoomMove
+            var zoomMoveOpts = structureOpts.zoomMove;
+            if (zoomMoveOpts && typeof zoomMoveOpts.zoom !== 'undefined') {
+              var center = o.getCenter(zoomMoveOpts.center);
+              var zoom = o.getZoom(zoomMoveOpts.zoom) + zoomMoveOpts.z_offSet;
+              stage.animationControls.zoomMove(center, zoom, zoomMoveOpts.duration);
+            }
+
+            // If in Shiny mode, collect AA sequences and other data
+            if (HTMLWidgets.shinyMode) {
+              var sequence = [];
+              var resno = [];
+              var chainname = [];
+              o.structure.eachResidue(rp => {
+                var code = rp.getResname1(); // The single-letter code
+                if (code !== 'X') {          // use `rp.resname` for 3-letter
+                  sequence.push(code);
+                  resno.push(rp.resno);
+                  chainname.push(rp.chainname);
+                }
+              });
+
+              // Write PDB
+              var writer = new NGL.PdbWriter(o.structure);
+              var PDBdata = writer.getData();
+              
+              // Collect data for all structures
+              sequences.push(sequence);
+              resnos.push(resno);
+              chainnames.push(chainname);
+              PDBdatas.push(PDBdata);
+              
+            }
+
+          });
+          
+                      
+          if (structures.length > 1 && opts.superpose.superpose) {
+              var refIndex = opts.superpose.reference - 1; 
+              var sRef = structures[refIndex].structure;
+              var seleRef = opts.superpose.seleReference;
+              var seleTarget = opts.superpose.seleTarget;
+
+              // Loop over all structures except the reference
+              for (var i = 0; i < structures.length; i++) {
+                  if (i !== refIndex) {
+                    var sTarget = structures[i].structure;
+                    // Superpose sTarget onto sRef
+                    NGL.superpose(sRef, sTarget, true, seleRef, seleTarget);
+                }
+                
+               }
+
+                // Update representations to reflect the new positions
+                structures[0].updateRepresentations({ position: true });
+          }
+          
+          if (structures.length > 1) {
+            stage.autoView();
+          }
+          
+          // Send data do shiny
+          if (HTMLWidgets.shinyMode) {
+
+              Shiny.onInputChange(`${el.id}_PDB`, PDBdatas);
+              
+              if (sequences.length > 1) {
+              sequences = sequences.map(seq => seq.join(''));
+              resnos = resnos.map(res => res.join(','));
+              chainnames = chainnames.map(chain => chain.join(''));
+              }
+              
+              Shiny.onInputChange(`${el.id}_sequence`, sequences);
+              Shiny.onInputChange(`${el.id}_resno`, resnos);
+              Shiny.onInputChange(`${el.id}_chainname`, chainnames);
+            }
+          
+      });
       
-      o.autoView();
-      //Set zoomMove
-      var zoomMoveOpts = opts.zoomMove
-      if(typeof(zoomMoveOpts.zoom) !== 'undefined'){
-      var center = o.getCenter(zoomMoveOpts.center)
-      var zoom = o.getZoom(zoomMoveOpts.zoom) + zoomMoveOpts.z_offSet;
-
-      stage.animationControls.zoomMove(center, zoom, zoomMoveOpts.duration);
-      }
-
-     //Send AA sequence to Shiny
-     //var aa = o.structure.getSequence()
-     //Shiny.onInputChange(`${el.id}_sequence`, aa);
-     if (HTMLWidgets.shinyMode) {
-     var sequence = [];
-     var resno = [];
-     var chainname = [];
-     o.structure.eachResidue( rp => {
-     var code = rp.getResname1() // The single-letter code, use `rp.resname` for 3-letter
-     if (code !== 'X') {
-       sequence.push(code);
-       resno.push(rp.resno);
-       chainname.push(rp.chainname);
-       }
-     })
-      
-     //Write PDB  
-      var writer = new NGL.PdbWriter(o.structure)
-      var PDBdata = writer.getData()
-      var PDBblob = writer.getBlob()
-
-     Shiny.onInputChange(`${el.id}_PDB`, PDBdata)  
-     Shiny.onInputChange(`${el.id}_sequence`, sequence)
-     Shiny.onInputChange(`${el.id}_resno`, resno)
-     Shiny.onInputChange(`${el.id}_chainname`, chainname)
-     }
-     });
-
      //<---------------------------// Shiny inputs // ---------------------------------->
      if (HTMLWidgets.shinyMode) {
-      //Send rendering status to shiny
-      stage.tasks.signals.countChanged.add(function(count){
+      // Send rendering status to shiny
+      stage.tasks.signals.countChanged.add(function(count) {
         if (count > 0) {
           Shiny.onInputChange(`${el.id}_rendering`, true);
-        } else
-        try {
-          Shiny.onInputChange(`${el.id}_rendering`, false);
-        } catch (e) {
-          //already removed
+        } else {
+          try {
+            Shiny.onInputChange(`${el.id}_rendering`, false);
+          } catch (e) {
+            // already removed
+          }
         }
       });
 
-      //Get AA selection details
-      stage.signals.clicked.add(function(pickingProxy){
-        if(pickingProxy) {
-        Shiny.onInputChange(`${el.id}_selection`, pickingProxy.getLabel());
+      // Click handler for the stage
+      stage.signals.clicked.add(function(pickingProxy) {
+        if (pickingProxy) {
+          // Get AA selection details
+          Shiny.onInputChange(`${el.id}_selection`, pickingProxy.getLabel());
+
+          // Get list of surrounding atoms
+          if (pickingProxy.atom) {
+            let atom = pickingProxy.atom;
+            let atomName = atom.qualifiedName().split("]").pop();
+            let resiName = atomName.split('.')[0];
+            let proximity = 3;
+            let proxlevel = "residue";
+
+            if (typeof(opts.selectionParameters) !== 'undefined') {
+              proximity = opts.selectionParameters["proximity"];
+              proxlevel = opts.selectionParameters["level"];
+            }
+
+            let selection = proxlevel === "residue" ? new NGL.Selection(resiName) : new NGL.Selection(atomName);
+            let selectedComponent = pickingProxy.component;
+            let selectedStructure = selectedComponent.structure;
+            
+            let atomSet = selectedStructure.getAtomSetWithinSelection(selection, proximity);
+            let atomSet2 = selectedStructure.getAtomSetWithinGroup( atomSet );
+            Shiny.onInputChange(`${el.id}_selAround`, atomSet2.toSeleString());
+          }
         }
       });
      }
@@ -148,7 +243,7 @@ HTMLWidgets.widget({
       },
 
       getStructure: function(){
-        return structure;
+        return structures;
       }
 
     };
@@ -205,22 +300,35 @@ Shiny.addCustomMessageHandler('NGLVieweR:updateFullscreen', function(message){
 
 });
 
-Shiny.addCustomMessageHandler('NGLVieweR:updateZoomMove', function(message){
-
-  var structure = getNGLStructure(message.id);
+Shiny.addCustomMessageHandler('NGLVieweR:updateZoomMove', function(message) {
+  
+  // Get the array of loaded structures
+  var structures = getNGLStructure(message.id);
   var stage = getNGLStage(message.id);
 
- if(typeof(structure) !== "undefined"){
+  // Check if structures are loaded and stage is defined
+  if (structures && structures.length > 0 && stage) {
+    var structureIndex = message.structureIndex;
 
-  structure.then(function(o){
-
-      var center = o.getCenter(message.center);
-      var zoom = o.getZoom(message.zoom) + message.z_offSet;
+    // Check if a specific structure index is provided
+    if (typeof structureIndex === 'number' && structureIndex >= 0 && structureIndex < structures.length) {
+      // Handle zoomMove for the specified structure
+      var structure = structures[structureIndex];
+      var center = structure.getCenter(message.center);
+      var zoom = structure.getZoom(message.zoom) + (message.z_offSet || 0);
 
       stage.animationControls.zoomMove(center, zoom, message.duration);
- })
-}
+    } else {
+      // No specific index provided; default to the first structure
+      var structure = structures[0];
+      var center = structure.getCenter(message.center);
+      var zoom = structure.getZoom(message.zoom) + (message.z_offSet || 0);
 
+      stage.animationControls.zoomMove(center, zoom, message.duration);
+    }
+  } else {
+    console.log("Structures not loaded, unavailable, or stage undefined.");
+  }
 });
 
 Shiny.addCustomMessageHandler('NGLVieweR:updateFocus', function(message){
@@ -253,18 +361,38 @@ Shiny.addCustomMessageHandler('NGLVieweR:removeSelection', function(message){
 
 });
 
-Shiny.addCustomMessageHandler('NGLVieweR:addSelection', function(message){
+Shiny.addCustomMessageHandler('NGLVieweR:addSelection', function(message) {
 
-  var structure = getNGLStructure(message.id);
+  // Get the array of loaded structures
+  var structures = getNGLStructure(message.id);
 
- if(typeof(structure) !== "undefined"){
+  // Check if structures are loaded
+  if (structures && structures.length > 0) {
 
-  structure.then(function(o){
+    // Prepare the representation parameters
+    var param = message.param;
+    var color = param['color'];
+    var structureIndex = param['structureIndex'];
+      
+    // Convert color if necessary
+    if (typeof color === 'object' && color !== null) {
+      param['color'] = colorMaker(param['color']);
+    }
+    
+    // Check if a specific structure index is provided
+    if (typeof structureIndex === 'number' && structureIndex >= 0 && structureIndex < structures.length) {
+      // Add representation to the specified structure
+      structures[structureIndex].addRepresentation(message.type, param);
+    } else {
+      // No specific index provided; add to all structures
+      structures.forEach(function(o) {
+        o.addRepresentation(message.type, param);
+      });
+    }
 
-  o.addRepresentation(message.type, message.param);
-
-  });
- }
+  } else {
+    console.log("Structures not loaded or unavailable.");
+  }
 });
 
 Shiny.addCustomMessageHandler('NGLVieweR:updateColor', function(message){
@@ -308,6 +436,14 @@ Shiny.onInputChange(`${message.id}_rendering`, false);
 Shiny.addCustomMessageHandler('NGLVieweR:updateRepresentation', function(message){
 
     var stage = getNGLStage(message.id);
+    
+    var param = message.param;
+    var color = param['color'];
+      
+      //Convert color values
+      if(typeof color === 'object' && color !== null){
+      param['color'] = colorMaker(param['color']);
+      }
 
 Shiny.onInputChange(`${message.id}_rendering`, true);
 setTimeout(function() {
